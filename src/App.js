62662f646edd903c7999aa761db2f7f29af04d6c
MinhVuongTran <minhvuongtran9x@gmail.com>
2023-01-13 08:55:41 +0700

    Adding a 'meals' component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Layouts/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
     return (
         <>
             <Header />
+            <Meals />
         </>
     );
 }
