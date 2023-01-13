51dbff9450481edc78594f215870e46220b4087c
MinhVuongTran <minhvuongtran9x@gmail.com>
2023-01-13 08:13:32 +0700

    Adding a 'header' component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from './components/Layouts/Header';
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    return (
+        <>
+            <Header />
+        </>
+    );
 }
 
 export default App;
